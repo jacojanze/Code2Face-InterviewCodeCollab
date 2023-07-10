17888111ca2e817bb4fddc6f5d0b5d7df1c07204
Vineeth Kumar Munigyala <munigyala@gmail.com>
2023-07-10 10:28:30 +0530

    update ReadMe

--- code2face/src/pages/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotFound = () => {
-  return (
-    <div className='errorpage'><span>Sorry, the request resources are not available on this website</span></div>
-  )
-}
-
-export default NotFound
