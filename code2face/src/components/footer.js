1ce5989e79d0837c02958d84a59848dc87f3c6a4
VineethKumarM <Munigyala@gmail.com>
2023-10-12 01:07:08 +0530

    Update footer.js by Rag

--- code2face/src/components/footer.js
+++ code2face/src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="page-footer font-small bg-dark text-center text-white pt-2">
 
         <div className="footer-copyright text-center py-3">
-            <h5>Made With ❤️ by Vineeth</h5>
+            <h5>Made With ❤️ by Raghava</h5>
         </div>
 
     </footer>
